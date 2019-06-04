f0d9da77d0e2f15a102cce6f1cf04d95abcc65f2
Stephen Wright <swright@dtimanagement.com>
2019-06-04 11:42:53 -0400

    adding in scrolling update

---
+++ dist/js/modules/addons.js
@@ -0,0 +1,27 @@
+/* Tabulator v4.2.3 (c) Oliver Folkerd */
+
+Cell.prototype.setOriginalValue = function (val) {
+    this.originalValue = val;
+};
+Cell.prototype.getOriginalValue = function () {
+    return this.originalValue;
+};
+Cell.prototype.clearOldValue = function () {
+    this.oldValue = null;
+};
+
+CellComponent.prototype.getOriginalValue = function () {
+    return this._cell.getOriginalValue();
+};
+CellComponent.prototype.clearOldValue = function () {
+    this._cell.clearOldValue();
+};
+CellComponent.prototype.restoreOriginalValue = function () {
+    this._cell.setValueActual(this._cell.getOriginalValue());
+};
+
+Tabulator.prototype.getColumnSort = function () {
+    if (this.modExists("persistence", true)) {
+        return this.modules.persistence.validateSorters(this.modules.sort.getSort());
+    }
+};
